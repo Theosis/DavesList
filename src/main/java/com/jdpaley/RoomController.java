1b0934598359e97a0991bdb66ec98bf461ed06d5
George Paley <George@JDPaley.com>
2017-06-27 17:10:40 -0400

    debug

---
+++ src/main/java/com/jdpaley/RoomController.java
@@ -0,0 +1,46 @@
+package com.jdpaley;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RoomController { 
+	
+	@Autowired
+	private RoomRepository roomRepository;
+	
+	@GetMapping(path="/")
+	public String list(@ModelAttribute("room") Room room, Model m) {
+		
+//		Room room = new Room();
+//		m.addAttribute("room", room);
+		
+		Iterable<Room> rooms = roomRepository.findAll();
+		m.addAttribute("list", rooms);
+		
+		return "index";
+
+	}
+
+	@PostMapping(path="/add")
+	public String add(@ModelAttribute("room") Room room, Model m) {
+		
+
+		roomRepository.save(room);
+		return "index";
+
+	}
+	
+	@GetMapping(path="/admin")
+	public String adminPage(Model m) {
+
+
+		return "admin";
+		
+	}
+
+}
